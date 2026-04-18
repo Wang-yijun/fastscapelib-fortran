df41e3b92a7522725b68360da60c09e5deaa917f
Wang-yijun <yijun0509wang@gmail.com>
2026-04-18 15:04:59 +0200

    make kds1 and kds2 arrays

--- src/FastScape_ctx.f90
+++ src/FastScape_ctx.f90
@@ -460,13 +460,8 @@ module FastScapeContext
 
   subroutine SetMarineParam (sl, p1, p2, z1, z2, r, l, kds1, kds2)
 
-    double precision, intent(in) :: sl, p1, p2, z1, z2, r, l, kds1, kds2
-
-    integer :: i
-    double precision :: hw
-
-    ! define decay coefficient (constant)
-    double precision, parameter :: lambda_s = 0.003d0
+    double precision, intent(in) :: sl, p1, p2, z1, z2, r, l
+    double precision, intent(in), dimension(*) :: kds1, kds2
 
     runMarine = .true.
 
@@ -478,22 +473,8 @@ module FastScapeContext
     ratio = r
     layer = l
 
-    do i = 1, nn
-
-      ! compute water depth
-      hw = sealevel - h(i)
-
-      if (hw > 0.d0) then
-        ! underwater: apply exponential decay
-        kdsea1(i) = kds1 * exp(-lambda_s * hw)
-        kdsea2(i) = kds2 * exp(-lambda_s * hw)
-      else
-        ! above sea level: keep original value
-        kdsea1(i) = kds1
-        kdsea2(i) = kds2
-      endif
-
-    enddo
+    kdsea1(1:nn) = kds1(1:nn)
+    kdsea2(1:nn) = kds2(1:nn)
 
     return
 
